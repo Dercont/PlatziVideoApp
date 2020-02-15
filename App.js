9539f08b358d36aec26bb85dc3541947cf6b5f15
Armando Almengor <armando.almengor@utp.ac.pa>
2020-02-14 22:03:22 -0500

    API

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import Home from './src/screens/containers/home';
+import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/header'; 
 import SuggestionList from './src/video/containers/suggestion-list';
+import API from './utils/api';
 
 export default class App extends React.Component{
+  componentDidMount(){
+    API.getSuggestion(10)
+  }
+
   render() {
   return (
     <Home>
